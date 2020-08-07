f6e1f2f460692136473e28e670fc3fec42844853
motty93 <rdwbocungelt5@gmail.com>
2020-08-07 12:45:17 +0900

    update: users CRUD

--- main.go
+++ main.go
@@ -2,10 +2,63 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+// User is struct
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var (
+	users   = map[int]*User{}
+	userSeq = 1
+)
+
+//----------
+// Handlers
+//----------
+
+func createUser(c echo.Context) error {
+	u := &User{
+		ID: userSeq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[user.ID] = u
+	userSeq++
+
+	return c.JSON(http.StatusCreated, user)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func updateUser(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+
+	return c.NoContent(http.StatusNoContent)
+}
 
 // hoge routing追加
 func hogeHandler(c echo.Context) error {
@@ -29,5 +82,12 @@ func main() {
 	e.GET("/test", testResponse)
 	// get hoge
 	e.GET("/hoge/:path", hogeHandler)
+
+	// users
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
 	e.Logger.Fatal(e.Start(":8020"))
 }
